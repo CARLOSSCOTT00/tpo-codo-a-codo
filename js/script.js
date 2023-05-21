b4f119b1e369be3cab9c27d1ffceca351f917384
carlos_scott00 <carlosdanielscott@gmail.com>
2023-05-21 11:30:24 -0300

    correccion de campos del formulario

--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ document.querySelectorAll(' .about .video-container .controls .control-btn').for
     document.querySelector('.about .video-container .video').src = src;
 }
 })
+
+const textarea = document.querySelector("textarea");
+textarea.addEventListener("keyup", e =>{
+    textarea.style.height ='${38}px';
+    let scHeight = e.target.scrollHeight;
+    console.log(scHeight);
+    textarea.style.height =`${scHeight}px`;
+});
